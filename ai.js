e7d34fc57f7740dea88e69a8af3e29b776364ec6
Quinn Webster <qwebster737@gmail.com>
2024-11-21 21:26:38 -0800

    fixed comments

--- ai.js
+++ ai.js
@@ -3,13 +3,12 @@ const systemPrompt = require("./systemPrompt.js");
 // import z from "zods";
 // import { zodResponseFormat } from "openai/helpers/zod";
 
-// const openai = new OpenAI({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 // Function to get AI-generated text based on the system prompt and syllabus
 async function getAiText(pdfText) {
-  return pdfText;
   try {
     const completion = await openai.chat.completions.create({
       model: "gpt-4o-mini",
